078a6b5835c9ecd0adda047023853526504d9d1f
Maciej <maciej.wereniewicz@student.put.poznan.pl>
2025-01-26 21:03:56 +0100

    tetris

--- main.py
+++ main.py
@@ -2,64 +2,26 @@ import RPi.GPIO as GPIO
 from time import sleep
 from sys import exit
 
-pin=32
+pin=32 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.B)
 GPIO.setup(pin, GPIO.OUT)
 p=GPIO.PWM(pin, 10)
 
 notes = {'C' : 523.25, 'D' : 587.33, 'E' : 659.25, 'F' : 349.23, 'G' : 392.00, 'A' : 440.00, 'B' : 493.88, '#' : 0.5, 'Ab' : 415.30}
-speed = {'sw' : 1,'w' : 0.8, 'h' : 0.6 , 'q' : 0.45, 'qh' : 0.25, 'qhh' : 0.15}
 
-s1 = ['E-q','B-qh','C-qh','D-q','C-qh','B-qh','A-q',
-'A-qh','C-qh','E-q','D-qh','C-q','B-qhh',
-'C-q','D-q','E-q','C-h','A-qh','A-q',
-'D-qh','F-qh','A-h','G-qhh','F-h','E-qh',
-'C-qh','E-h','D-qhh','C-h','B-qh',
-'B-qh','C-q','D-q','E-qh','C-h','A-h','A-h','#-w']
-
-s2 = ['E-sw','C-sw','D-sw','B-sw','C-sw','A-sw','Ab-sw','B-w','#-q',
-'E-sw','C-sw','D-sw','B-sw','C-h','A-h','A-h','Ab-sw','#-sw']
-
-tetris = [s1,s1,s2,s1,s1,s2,s1]
+tetris = ["E","B","C","D","C","B","A","A","C","E","D","C","B","C","D","E","C","A","A","D","F","A","G","F","E","C","E","D","C","B","B","C","D","E","C","A","A","","E","B","C","D","C","B","A","A","C","E","D","C","B","C","D","E","C","A","A","D","F","A","G","F","E","C","E","D","C","B","B","C","D","E","C","A","A","","E","C","D","B","C","A","Ab","B","E","C","D","B","C","E","A","Ab","","E","B","C","D","C","B","A","A","C","E","D","C","B","C","D","E","C","A","A","D","F","A","G","F","E","C","E","D","C","B","B","C","D","E","C","A","A","","E","B","C","D","C","B","A","A","C","E","D","C","B","C","D","E","C","A","A","D","F","A","G","F","E","C","E","D","C","B","B","C","D","E","C","A","A","E","C","D","B","C","A","Ab","B","E","C","D","B","C","E","A","Ab"]
 
 p.start(50)
 
-header = '[Note]\t[Frequency]\t[Duration]'+'\n------\t-----------\t----------'
-printcount = 0
+def play_sound(sound):
+    p.ChangeFrequency(notes[sound])
+    sleep(0.2)
+    p.ChangeFrequency(10)
+	
 
-def goodbye(msg = ''):
-	print(msg + '\n\nDone! Have fun. Like. Comment below. Kill yourself.')
-	exit()
-
-def play_song(song):# -----------------------------------------------------------------------------
-# If you're reading this, you shouldn't be here
-# unless you know what you are doing.
-# -----------------------------------------------------------------------------
-#Duty cycle of 50% does his job
-	global header,printcount,notes,speed
-	try:
-		print(header)
-		songposition = 0
-		for x in song:
-			songposition += 1
-			if(songposition >= (len(song))):
-				goodbye('\nSong finished...')
-			for y in x:
-				y = y.split('-')
-				note = y[0]
-				length = y[1]
-				actualspeed = round(float(speed[length]),2)
-				print('  '+note + '\t   ' + str(notes[note]) + '\t   ' + str(actualspeed))
-				printcount += 1
-				if(printcount == 23):
-					printcount = 0
-					print(header)
-				p.ChangeFrequency(notes[note])
-				sleep(actualspeed)
-				p.ChangeFrequency(10)
-	except KeyboardInterrupt:
-		goodbye()
-
+def play_song(song):
+	for sound in song:
+		play_sound(sound)
 while True:
 	play_song(tetris)
